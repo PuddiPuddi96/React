3dda630b1c1d206acff83a5d187bfd259c63ce67
davide.strianese <davide.strianese@itsvil.it>
2024-05-20 11:14:02 +0200

    conf: configuration environment

---
+++ Book-Store/backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const MONGODB_URL = 
+    'mongodb+srv://admin:admin@testcluster.a4nc919.mongodb.net/books-collection?retryWrites=true&w=majority&appName=TestCluster';
