959fb799c2d3df630d891d15e685f9eb9f5b6b20
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-05-21 12:44:44 +0200

    fix: fix CORS

--- Book-Store/backend/index.js
+++ Book-Store/backend/index.js
@@ -13,17 +13,17 @@ app.use(express.json());
 
 //Middleware for handling CORS policy
 
-//Allow all
-//app.use(cors());
+//Allows all
+app.use(cors());
 
 //Allow custom origins
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type']
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type']
+//     })
+// );
 
 app.get('/', (request, response) => {
     console.log(request);
