853b92feb0ccb1a99c9cda3138379dea4f30d1e7
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-06-10 14:24:47 +0200

    dev: implement signup

--- pern-chat-app/backend/src/controllers/auth.controller.ts
+++ pern-chat-app/backend/src/controllers/auth.controller.ts
@@ -1,6 +1,63 @@
 import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
+import bcryptjs from 'bcryptjs';
+import generateToken from "../utils/generateToken.js";
 
 export const signup = async (request: Request, response: Response) => {
+  try {
+    const {fullname, username, password, confirmPassword, gender} = request.body;
+
+    if(!fullname || !username || !password || !confirmPassword || !gender){
+      return response.status(400).json({error: "Please fill in all fields"});
+    }
+
+    if(password !== confirmPassword) {
+      return response.status(400).json({error: "Passwords don't match"});
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        username
+      }
+    });
+
+    if(user){
+      return response.status(400).json({error: "Username already exists"});
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullname,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
+      }
+    })
+
+    if(newUser){
+      generateToken(newUser.id, response)
+
+      response.status(201).json({
+        id: newUser.id,
+        fullname: newUser.fullname,
+        username: newUser.username,
+        profilePic: newUser.profilePic
+      })
+    }else{
+      return response.status(400).json({error: "Invalid user data"});
+    }
+
+  } catch (error: any) {
+    console.log('Error in signup controller', error.message);
+    return response.status(500).json({error: "Internal server error"});
+  }
 
 };
 
