b46d9addf28bcda562eb11549165fc618085db10
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-06-20 17:36:56 +0200

    dev: implement and configuration socket (BE)

--- pern-chat-app/backend/src/index.ts
+++ pern-chat-app/backend/src/index.ts
@@ -3,9 +3,9 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js'
 import dotnev from 'dotenv'
 import cookieParser from 'cookie-parser';
+import { app, server } from './socket/socket.js';
 
 dotnev.config();
-const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cookieParser());
@@ -18,6 +18,6 @@ app.get('/', (request: Request, response: Response) => {
   response.send('Hello world2!');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`)
 });
